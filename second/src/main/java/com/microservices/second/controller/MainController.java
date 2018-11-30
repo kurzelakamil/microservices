884913bd917ef3bcfa723815b3fde20b31f1b2a9
Kamil Kurzela <kurzela.kamil@gmail.com>
2018-11-30 22:50:54 +0100

    ap

--- second/src/main/java/com/microservices/second/controller/MainController.java
+++ second/src/main/java/com/microservices/second/controller/MainController.java
@@ -1,20 +1,32 @@
 package com.microservices.second.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class MainController {
 
+	@Value("${desc}")
+	private String string;
+	
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
-	@GetMapping
+	@GetMapping("/")
 	public String sendResponse() {
-		return "hello World";
+		return string;
+	}
+	
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
 	}
 }
