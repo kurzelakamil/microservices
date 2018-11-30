d0b72c43a3d4d1b3313b4e0f6c3fff9ab3d72e78
Kamil Kurzela <kurzela.kamil@gmail.com>
2018-11-30 22:31:06 +0100

    sec service props

--- second/src/main/java/com/microservices/second/controller/MainController.java
+++ second/src/main/java/com/microservices/second/controller/MainController.java
@@ -3,15 +3,17 @@ package com.microservices.second.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class MainController {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
-	@GetMapping("/")
+	@GetMapping
 	public String sendResponse() {
 		return "hello World";
 	}
