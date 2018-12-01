fb72941680237ed5871517ac86fe3c1d3b170d41
Kamil Kurzela <kurzela.kamil@gmail.com>
2018-12-01 10:34:16 +0100

    ap

--- second/src/main/java/com/microservices/second/controller/MainController.java
+++ second/src/main/java/com/microservices/second/controller/MainController.java
@@ -3,7 +3,6 @@ package com.microservices.second.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-	
+
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
 	@GetMapping("/")
 	public String sendResponse() {
 		return "hello world";
+	}
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
 	}
 }
